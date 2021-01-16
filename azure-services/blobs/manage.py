473cb36bad0237d64864aba306ba3ae99bbefdf4
o.nikonov <o.nikonov@flex-solution.com>
2021-01-16 19:04:26 +0200

    react initiated

---
+++ azure-services/blobs/manage.py
@@ -0,0 +1,25 @@
+import os
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
+# add variable 
+# export AZURE_STORAGE_CONNECTION_STRING="pSWYOvxWVJINthORcmX2Yp9hPiU3TarugY9Ipd2f/+4r08pbkTiWTIHVA1SWOYT86+0qMwlesdeBGwar/0ZOvg=="
+# https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python
+# Retrieve the connection string for use with the application. The storage
+# connection string is stored in an environment variable on the machine
+# running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
+# created after the application is launched in a console or with Visual Studio,
+# the shell or application needs to be closed and reloaded to take the
+# environment variable into account.
+class BlobManager:
+    def __init__(self, uploading_files):
+        self.azure_credentials: ''
+        self.uploading_files: uploading_files
+        self.stored_files: {}
+        connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+        # Create the BlobServiceClient object which will be used to create a container client
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+    def upload_files(self):
+        for name, value in self.uploading_files.items():
+            name; value
+
