fc67c1807d3358d423e245d3f9b16307745c46ca
o.nikonov <o.nikonov@flex-solution.com>
2021-02-13 23:24:49 +0200

    configs added for back processor added inputs in front weather app

--- azure_services/function_service.py
+++ azure_services/function_service.py
@@ -1,4 +1,4 @@
-from flask import request
+import requests
 import os
 
 LOCAL_URL = 'http://localhost:7071/api/weather_trigger'
@@ -10,10 +10,12 @@ DEVELOPMENT_NAME = 'development'
 class FunctionService:
   def __init__(self):
     self.city = '?'
+    self.api_key = '785050a8bbb48b3b77d73bc0c6704bd7'
 
-  def get_weather(self):
-    url = LOCAL_URL if os.environ(DEVELOPMENT_VARIABLE) == DEVELOPMENT_NAME else AZURE_PORTAL_URL
-    return request.get(url)
+  def get_weather(self, lat, long):
+    url = LOCAL_URL if os.getenv(DEVELOPMENT_VARIABLE) == DEVELOPMENT_NAME else AZURE_PORTAL_URL
+    result = requests.get(self._get_weather_url(lat, long))
+    pass
 
   def interrogate_weather_server(self):
     pass
--- weather_processor/processor.py
+++ weather_processor/processor.py
@@ -1,7 +1,7 @@
 from . import weather_processor
 import azure.functions as azure_func
 
-class WeatherProcessor:
-    def __init__(self):
-        pass
 
+class WeatherProcessor:
+  def __init__(self):
+    pass
