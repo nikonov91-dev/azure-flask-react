df142163b39e4c552143948ade7032488b2fb7dd
o.nikonov <o.nikonov@flex-solution.com>
2021-02-20 11:52:38 +0200

    front-app and server deployment variables change

--- front-app/start.sh
+++ front-app/start.sh
@@ -6,9 +6,9 @@ REACT_DEV_FRONT_APP_URL: $(secrets.REACT_DEV_FRONT_APP_URL)
 TEST: $(secrets.TEST)
 " > .env
 echo "dev url:"
-echo "$(DEV_SERVER_URL)"
+echo "$DEV_SERVER_URL"
 echo "first line test"
-echo "$(secrets.DEV_SERVER_URL)"
+echo "$secrets.DEV_SERVER_URL"
 echo "second line test"
-echo "$(env.secrets.DEV_SERVER_URL)"
+echo "$env.secrets.DEV_SERVER_URL"
 echo "finish"
