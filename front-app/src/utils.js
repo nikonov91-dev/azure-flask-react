a61e91c482055c1a376d62ac91f71bd47e9a1fb8
o.nikonov <o.nikonov@flex-solution.com>
2021-02-24 10:25:55 +0200

    env cmd

--- front-app/src/utils.js
+++
@@ -1,7 +0,0 @@
-
-export const getFrontAppUrl = () => {
-  debugger;
-  return process.env.REACT_DEV_FRONT_APP_URL ? process.env.REACT_DEV_FRONT_APP_URL : '';
-};
-
-export const getServerUrl = () => process.env.DEV_SERVER_URL ? process.env.DEV_SERVER_URL : '127.0.0.1.5000/';
